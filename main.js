122fe9dfe455d1341a687c69c35bf9b39ab95ae0
Cudanglun13759350 <v.ducphong@gmail.com>
2021-10-24 20:11:08 +0300

    fixing PORT for deploying heroku

--- main.js
+++ main.js
@@ -50,7 +50,8 @@ router.render = (req, res) => {
 };
 
 // Use default router
-server.use(router);
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.use('/api', router);
+server.listen(PORT, () => {
 	console.log('JSON Server is running');
 });
